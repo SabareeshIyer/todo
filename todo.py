4f7e17e88b12ace0b73bf5f08819fbc800bb856a
SabareeshIyer <sabareeshmb@gmail.com>
2016-11-03 17:17:45 +0530

    Added new functionality

--- todo.py
+++ todo.py
@@ -1,6 +1,6 @@
 import os
 
-def addtask(comm):
+def addTask(comm):
 	file = open('tasklist.txt','a')
 	if(os.stat("tasklist.txt").st_size != 0):
 		file.write('\n')
@@ -9,14 +9,67 @@ def addtask(comm):
 	file.close()
 	return
 
-def printtasks():
+'''def printtasks():
 	file = open('tasklist.txt','r')
 	op = file.readlines()
 	for o in op:
 		print(o,end='')
 	file.close()
 	return
+'''
 
+def showTasks():
+	file = open('tasklist.txt','r')
+	i = 1
+	op = file.readlines()
+	for o in op:
+		print(i,o,end='')
+		i = i+1
+	file.close()
+	return
+
+def deleteTasks(linenum):
+	file = open('tasklist.txt','r+')
+	d = file.readlines()
+	numb = int(linenum)
+	file.seek(0)
+	count = 1
+	for i in d:
+		if(count != numb):
+			file.write(i)
+		count = count +1
+	file.truncate()
+	file.close()
+	return
+
+def editTask(linenum, desc):
+	file = open('tasklist.txt','r+')
+	d = file.readlines()
+	numb = int(linenum)
+	file.seek(0)
+	count = 1
+	for i in d:
+		if(count != numb):
+			file.write(i)
+		if(count == numb):
+			file.write(desc+'\n')
+		count = count +1
+	file.truncate()
+	file.close()
+	return
+
+
+def bulkDelete():
+	return
+
+def man():
+	print('Command(s)	:		Description\n'+
+			'add 		:		Add new task\n'+
+			'del, delete	:		Delete task\n'+
+			'bdel 		:		Bulk delete\n'+
+			'show 		:		Show task list\n'+
+			'edit 		:		Edit task\n')
+	return
 
 stop = False
 ip = input()
@@ -24,10 +77,20 @@ command = ip.split()
 
 
 while(stop == False):
-	if(command[0] == 'add'):
-		addtask(command)
+	case = command[0]
+	if(case == 'add'):
+		addTask(command)
+	elif(case == 'show'):
+		showTasks()
+	elif(case == 'del' or case == 'delete'):
+		deleteTasks(command[1])
+	elif(case == 'edit'):
+		editTask(command[1],command[2])
+	elif(case == 'man'):
+		man()
+
 	ip = input()
 	command = ip.split()
 	if(ip=='exit'):
 		stop = True
-		printtasks()
+		#printtasks()
